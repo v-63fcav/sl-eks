a90e1028ee80de40230d959d7625c86d0d3cb3d0
v-63fcav <v-63fcav@hotmail.com>
2026-03-12 18:33:51 -0300

    cleanup for tomorrow 2

--- apps/helm.tf
+++ apps/helm.tf
@@ -11,38 +11,3 @@ resource "helm_release" "alb_controller" {
     })
   ]
 }
-
-# AWS Distro for OpenTelemetry Collector - Replaces Prometheus
-resource "helm_release" "adot_collector" {
-  name       = "adot-collector"
-  repository = "https://aws-observability.github.io/helm-charts"
-  chart      = "opentelemetry-collector"
-  version    = "0.89.0"
-
-  namespace        = "observability"
-  create_namespace = true
-  cleanup_on_fail  = true
-
-  values = [
-    templatefile("${path.module}/values/values-adot.yaml", {
-      adot_irsa_role = local.adot_irsa_role
-      amp_endpoint   = aws_prometheus_workspace.main.prometheus_endpoint
-    })
-  ]
-
-  depends_on = [aws_prometheus_workspace.main]
-}
-
-# Blackbox Exporter - kept for ADOT integration
-resource "helm_release" "blackbox" {
-  name       = "blackbox-exporter"
-  namespace  = "observability"
-  create_namespace = false
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "prometheus-blackbox-exporter"
-  version    = "8.8.0"
-
-  values = [
-    file("${path.module}/values/values-blackbox.yaml")
-  ]
-}
