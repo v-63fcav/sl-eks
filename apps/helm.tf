1689f29f2a43860556fda4e0c41ccd909f0dea40
v-63fcav <v-63fcav@hotmail.com>
2026-03-12 17:42:07 -0300

    refactor: comment out unused Helm release resources in helm.tf

--- apps/helm.tf
+++ apps/helm.tf
@@ -12,67 +12,67 @@ resource "helm_release" "alb_controller" {
   ]
 }
 
-resource "helm_release" "prometheus_stack" {
-  name       = "prometheus-stack"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-  version    = "47.6.1"
+# resource "helm_release" "prometheus_stack" {
+#   name       = "prometheus-stack"
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   chart      = "kube-prometheus-stack"
+#   version    = "47.6.1"
 
-  namespace        = "prometheus-stack"
-  create_namespace = true
-  cleanup_on_fail  = true
+#   namespace        = "prometheus-stack"
+#   create_namespace = true
+#   cleanup_on_fail  = true
 
-  values = [
-    "${file("${path.module}/values/values-prometheus.yaml")}"
-  ]
+#   values = [
+#     "${file("${path.module}/values/values-prometheus.yaml")}"
+#   ]
 
-  depends_on = [
-    kubernetes_storage_class_v1.gp3
-  ]
-}
+#   depends_on = [
+#     kubernetes_storage_class_v1.gp3
+#   ]
+# }
 
-resource "helm_release" "blackbox" {
-  name       = "blackbox-exporter"
-  namespace  = "monitoring"
-  create_namespace = true
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "prometheus-blackbox-exporter"
-  version    = "8.8.0"
+# resource "helm_release" "blackbox" {
+#   name       = "blackbox-exporter"
+#   namespace  = "monitoring"
+#   create_namespace = true
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   chart      = "prometheus-blackbox-exporter"
+#   version    = "8.8.0"
 
-  values = [
-    file("${path.module}/values/values-blackbox.yaml")
-  ]
-}
+#   values = [
+#     file("${path.module}/values/values-blackbox.yaml")
+#   ]
+# }
 
-resource "helm_release" "metrics_server" {
-  name       = "metrics-server"
-  repository = "https://kubernetes-sigs.github.io/metrics-server"
-  chart      = "metrics-server"
-  version    = "3.12.2"
+# resource "helm_release" "metrics_server" {
+#   name       = "metrics-server"
+#   repository = "https://kubernetes-sigs.github.io/metrics-server"
+#   chart      = "metrics-server"
+#   version    = "3.12.2"
 
-  namespace  = "kube-system"
+#   namespace  = "kube-system"
   
-  values = [
-    file("${path.module}/values/values-metrics-server.yaml")
-  ]
-}
+#   values = [
+#     file("${path.module}/values/values-metrics-server.yaml")
+#   ]
+# }
 
-# Sample application with gp3 volume mount
-resource "helm_release" "sample_app" {
-  name       = "sample-app"
-  namespace  = "default"
-  create_namespace = false
+# # Sample application with gp3 volume mount
+# resource "helm_release" "sample_app" {
+#   name       = "sample-app"
+#   namespace  = "default"
+#   create_namespace = false
 
-  chart = "${path.module}/sample-app-chart"
+#   chart = "${path.module}/sample-app-chart"
   
-  timeout = 600
-  atomic  = true
+#   timeout = 600
+#   atomic  = true
 
-  values = [
-    file("${path.module}/sample-app-chart/values.yaml")
-  ]
+#   values = [
+#     file("${path.module}/sample-app-chart/values.yaml")
+#   ]
 
-  depends_on = [
-    kubernetes_storage_class_v1.gp3
-  ]
-}
+#   depends_on = [
+#     kubernetes_storage_class_v1.gp3
+#   ]
+# }
