0248ff1bb71134fabee932d9b3442afe5a475b0e
v-63fcav <v-63fcav@hotmail.com>
2026-03-13 14:11:10 -0300

    feat: add Loki, Tempo, and OpenTelemetry Collector Helm releases with configurations

--- apps/helm.tf
+++ apps/helm.tf
@@ -27,3 +27,39 @@ resource "helm_release" "kube_prometheus_stack" {
 
   depends_on = [kubernetes_storage_class_v1.gp3]
 }
+
+resource "helm_release" "loki" {
+  name       = "loki"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "loki"
+  version    = "6.29.0"
+  namespace  = "monitoring"
+
+  values = [file("${path.module}/values/values-loki.yaml")]
+
+  depends_on = [kubernetes_storage_class_v1.gp3, helm_release.kube_prometheus_stack]
+}
+
+resource "helm_release" "tempo" {
+  name       = "tempo"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "tempo"
+  version    = "1.14.0"
+  namespace  = "monitoring"
+
+  values = [file("${path.module}/values/values-tempo.yaml")]
+
+  depends_on = [kubernetes_storage_class_v1.gp3, helm_release.kube_prometheus_stack]
+}
+
+resource "helm_release" "opentelemetry_collector" {
+  name       = "opentelemetry-collector"
+  repository = "https://open-telemetry.github.io/opentelemetry-helm-charts"
+  chart      = "opentelemetry-collector"
+  version    = "0.118.0"
+  namespace  = "monitoring"
+
+  values = [file("${path.module}/values/values-otel-collector.yaml")]
+
+  depends_on = [helm_release.loki, helm_release.tempo, helm_release.kube_prometheus_stack]
+}
